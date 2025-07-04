395a44c03c14a6df6a219e149b8e15d047a1b427
dwrigley1 <dwrigley1@ivytech.edu>
2025-07-04 07:58:35 -0400

    created the first API route to get songs

--- app.js
+++ app.js
@@ -6,15 +6,20 @@
 
 // setup... this is similar to when we use our default tags in HTML
 const express = require("express")
+var cors = require('cors') // have to use to host frontend & backend on same device
 
 //activate or tells this app variable to be an express server
 const app = express()
+app.use(cors()) // allows to use cors system to connect backend & frontend
 const router = express.Router()
 
-// start the web server.. app.listen(portnumber, function)
+
+/** commented out because we are using nodemon instead of spinning up the server after each file change 
+start the web server.. app.listen(portnumber, function)
 app.listen(3000,function(){
     console.log("Listening on port 3000")
-})
+}
+**/
 
 // making an api using routes
 // routes are used to handle browser requests. They look like URLs. The difference is thsat when a browser requests a route, it is dynamically handled by using a function
@@ -28,3 +33,27 @@ app.get("/hello",function(req,res){
 app.get("/Goodbye",function(req,res){
     res.send("<h1>Goodbye Express!!!</h1>")
 })
+
+router.get("/songs",function(req,res){
+    const songs = [
+        {
+        title: "Uptown Funk",
+        artist: "Bruno Mars",
+        popularity: 10,
+        releaseDate: new Date (2011, 9, 22),
+        genre: ["funk", "boogie"]
+    },
+    {
+        title: "We Found Love",
+        artist: "Rihanna",
+        popularity: 10,
+        releaseDate: new Date(2013, 11, 21),
+        genre: ["electro house"]
+    }
+];
+    res.json(songs)
+})
+
+// all requests that usually use an API start with /api...so the URL would be localhost:3000/api/songs
+app.use("/api",router) // this line instructs the script to use the /api URL before using one wihtout it
+app.listen(3000)
