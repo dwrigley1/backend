0de0768c24849e5c275146562f0d2735d9417471
dwrigley1 <dwrigley1@ivytech.edu>
2025-07-20 14:59:37 -0400

    added db

--- app.js
+++ app.js
@@ -1,8 +1,3 @@
-// npm init
-// 'enter' through options for default/blank values
-// 'yes'
-// node app.js -> Listening on port 3000 -> localhost:3000/hello
-
 // setup... this is similar to when we use our default tags in HTML
 
 const express = require("express")
@@ -26,6 +21,19 @@ router.get("/songs", async(req,res) =>{
   }
 })
 
+router.get("/songs/:id", async (req, res) => {
+  try {
+    const song = await Song.findById(req.params.id);
+    if (!song) {
+      return res.status(404).send("Song not found");
+    }
+    res.send(song);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Server error");
+  }
+});
+
 router.post("/songs", async(req,res)=>{
   try{
     const song = await new Song(req.body)
@@ -37,28 +45,29 @@ router.post("/songs", async(req,res)=>{
     res.status(400).send(err)
   }
 })
-  // to find all songs in a database, just use find() that is built in to Mongoose
-  /** 
-  Song.find(query,function(err,songs){
-    if(err){
-      res.status(400).send(err)
-    }
-    else{
-      res.json(songs)
-    }
-  })
-    **/
 
-/** commented out because we are using nodemon instead of spinning up the server after each file change 
-start the web server.. app.listen(portnumber, function) **/
+// put request updates an existing record database entry
+// app.js
+router.put("/songs/:id", async (req, res) => {
+  try {
+    const song = req.body;
+    await Song.updateOne({ _id: req.params.id }, song);
+    console.log("Updated:", song);
+    res.sendStatus(204); // 204 = No Content, meaning success
+  } catch (err) {
+    console.error("Update error:", err);
+    res.status(400).send(err);
+  }
+});
+
+
+// the slash is in the URL
+
+
 app.listen(3000,function(){
     console.log("Listening on port 3000")
 })
 
-// making an api using routes
-// routes are used to handle browser requests. They look like URLs. The difference is thsat when a browser requests a route, it is dynamically handled by using a function
-// GET or a regular request when someone goes to http://localhost:3000/hello (3000 isn't neccessary, it is the default). 
-// When using a function and a route, we almost always have a parameter or handle a response and request
 
 router.get("/songs", (req, res) => {
   const songs = [
@@ -70,13 +79,12 @@ router.get("/songs", (req, res) => {
   res.json(songs);
 });
 
+
+app.use("/api",router) // this line instructs the script to use the /api URL before using one wihtout it
+
 // below was added for Glitch
 //const PORT = process.env.PORT || 3000;
 //app.listen(PORT, () => {
   //console.log(` Server running on port ${PORT}`);
 //});
 
-// all requests that usually use an API start with /api...so the URL would be localhost:3000/api/songs
-
-app.use("/api",router) // this line instructs the script to use the /api URL before using one wihtout it
-//app.listen(3000)
